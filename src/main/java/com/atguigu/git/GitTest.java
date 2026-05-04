a3ce30d9fb92f48f5e01ab07bf70b5376722bd7d
Vincent <Vincent@atguigu.com>
2026-05-04 11:40:41 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,18 @@
+package com.atguigu.git;
+
+/**
+ * ClassName:GitTest
+ * Package:com.atguigu.git
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("master test");
+        System.out.println("hot-fix test");
+        System.out.println("push test");
+        System.out.println("pull test");
+    }
+}
