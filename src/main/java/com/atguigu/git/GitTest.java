2ac36fd54ec2cfddf12656482a8b87739c8d0729
Vincent <Vincent@atguigu.com>
2026-05-04 13:34:03 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,9 +10,5 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git");
         System.out.println("hello git4");
-        System.out.println("master test");
-        System.out.println("hot-fix test");
-        System.out.println("push test");
-        System.out.println("pull test21");
     }
 }
